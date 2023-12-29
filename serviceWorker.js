c98deade5a7167566774faacaecccd4a2ac2fc74
Yash Hegde <yash4success@gmail.com>
2023-12-29 10:19:08 +0530

    register valid service worker added

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,8 @@
 //variable to check if the web app is being run from a localhost server
 // or local development environment
 
+import e from "cors";
+
 const islocalHost = Boolean(
     window.location.hostname === 'localhost' || window.location.hostname === '[::1]' || window.location.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/)
     //localhost or ipv6 or ipv4
@@ -48,8 +50,36 @@ export async function register(config) {
 
 
 
-registerValidSW(){
+async function registerValidSW(swUrl, config){
+    try {   
+        const registration = await navigator.serviceWorker.register(swUrl);
 
+        registration.onupdatefound = () => {
+            const installWorker = registration.installing;
+            if (installWorker == null) {
+                return;
+            }
+            installWorker.onstatechange = () => {
+                if (installWorker.state === 'installed') {
+                    if (navigator.serviceWorker.controller) {
+                        console.log('New content is available and will be used when all tabs for this page are closed. See http://bit.ly/CRA-PWA.');
+                        if (config && config.onUpdate) {
+                            config.onUpdate(registration);
+                        }
+                    } else {
+                        console.log('Content is cached for offline use.');
+                        if (config && config.onSuccess) {
+                            config.onSuccess(registration);
+                        }
+                    }
+                }
+            };
+
+        };
+        
+    }catch(error){
+        console.log('Error during service worker registration:', error);
+    }
 }
 
 async function checkValidServiceWorker(swUrl, config) {
